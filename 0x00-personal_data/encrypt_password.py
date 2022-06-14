a0e5d0addca915322c8cb554f12a7c0158ccf284
Afonne-CID <delightinbusiness@gmail.com>
2022-06-14 00:49:21 -0700

    fix spelling error

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -15,4 +15,4 @@ def hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     '''Verify password
     '''
-    return bcrupt.checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
