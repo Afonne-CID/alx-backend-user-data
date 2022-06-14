d97d97d6b85807343b90b9eea8ae63c631285b70
Afonne-CID <delightinbusiness@gmail.com>
2022-06-14 00:46:09 -0700

    encrypt and decrypt password

---
+++ encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Encrypting Passwords
+'''
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''Salted password generation
+    '''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Verify password
+    '''
+    return bcrupt.checkpw(password.encode('utf-8'), hashed_password)
